10dce5313bdf5fd3cdb49c07ed250cf2d55402b1
misomosi <misomosispi@gmail.com>
2024-08-10 15:31:57 -0400

    Map sources to sites

--- index.js
+++ index.js
@@ -19,9 +19,14 @@ const lex = (query) => {
 
 document.getElementById('query').addEventListener('submit', (ev) => {
 	ev.preventDefault();
-	element = ev.submitter;
+	submitter = ev.submitter;
+
+	sites = Object.entries(sourcesTranslation)
+		.filter(entry => document.getElementById('source_'+entry[0]).checked)
+		.map(entry => `site:${entry[1]}`);
+
 	as_q = document.getElementById('as_q').value || '';
 	tokens = lex(as_q);
-	query = encodeURIComponent(`(site:npr.org OR site:nytimes.com) ${tokens.join(' ')}`)
+	query = encodeURIComponent(`(${sites.join(' OR ')}) ${tokens.join(' ')}`)
 	window.location.href = 'https://google.com/search?q=' + query;
-})
+});
